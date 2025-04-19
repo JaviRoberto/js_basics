fb72b44c19a553a6fe7c08f55c3f85eed3a93e35
jochoa70 <jocho70@wgu.edu>
2025-04-19 12:42:42 +0200

    add operators

--- index.js
+++ index.js
@@ -1,15 +1,38 @@
 document.addEventListener('DOMContentLoaded',function(){
     const form= document.getElementById("calculator"); 
 
-    form.addEventListener('submit', function(event){ 
+    form.addEventListener("submit", function(event){ 
         event.preventDefault();
 
         let first_number = parseFloat(document.getElementById('first_number').value); 
         let second_number = parseFloat(document.getElementById('second_number').value); 
 
-        result_number = first_number + second_number; 
-        console.log(result_number)
-        formuala_string = first_number + " and " + second_number + " equal..."
+        let operator = document.getElementById("operation").value
+        console.log(operator)
+
+        
+        switch (operator){
+            case 'add': 
+                formuala_string = first_number + " and " + second_number + " equal..."; 
+                result_number = first_number + second_number; 
+                break; 
+            case 'subtract': 
+                formuala_string = first_number + " minus " + second_number + " equal..."; 
+                result_number = first_number - second_number; 
+                break; 
+            case 'mutliply': 
+                formuala_string = first_number + " mutliplied by " + second_number + " equal..."; 
+                result_number = first_number * second_number; 
+                break;
+            case 'devide': 
+                formuala_string = first_number + " divided by " + second_number + " equal..."; 
+                result_number = first_number / second_number; 
+                break; 
+            default: 
+                console.log("Error")
+
+        }
+
 
         document.getElementById('formula').textContent = formuala_string; 
         document.getElementById('results').textContent =result_number;
